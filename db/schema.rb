d700219b5a5d3e46af9c194791d56fbec5a5b74f
robmadden <rob.madden@bloc.io>
2016-02-20 19:09:08 -0800

    Voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221022354) do
+ActiveRecord::Schema.define(version: 20160221030647) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160221022354) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -52,5 +53,16 @@ ActiveRecord::Schema.define(version: 20160221022354) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,17 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
-      user:   users.sample,
-      topic:  topics.sample,
-      title:  RandomData.random_sentence,
-      body:   RandomData.random_paragraph
+  post = Post.create!(
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
+
 posts = Post.all
 
 # Create Comments
@@ -60,3 +64,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
